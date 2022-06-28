11f6ecd8f2bb68acbd9af4911e0186e9a698f9d7
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-28 14:39:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+  static titleize (string){
+    if (string !== "the" && string !== "at" && string !== "a" && string !== "but" && string !== "but" && string !== "of" && string !== "and" && string !== "for" && string !== "at" && string !== "by" && string !== "from") {
+      return string.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())
+    }
+     
+  }
 }
